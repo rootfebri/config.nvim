8a3fa9808c4a56437598526a8daeb24638f79e06
Febri <root.febri@gmail.com>
2026-02-27 06:36:22 +0700

    Auto Commit

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,63 +1,76 @@
-return {
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
+local config = {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
     },
-    opts = {
-      notify_on_error = true,
-      format_on_save = function()
-        if vim.fn.has 'win32' == 0 then
-          return {
-            timeout_ms = 1000,
-            lsp_format = 'fallback',
-            async = true,
-          }
-        else
-          return {}
-        end
-      end,
-      format_after_saved = function()
-        if vim.fn.has 'win32' == 1 then
-          return {}
-        else
-          return {
-            timeout_ms = 1000,
-            lsp_format = 'fallback',
-            async = true,
-          }
-        end
-      end,
-      formatters_by_ft = {
-        rust = { 'rustfmt', lsp_format = 'fallback' },
-        php = { 'pint' },
-        lua = { 'stylua' },
-        javascript = { 'prettier', stop_after_first = true },
-        javascriptreact = { 'prettier', stop_after_first = true },
-        typescript = { 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettier', stop_after_first = true },
-        json = { 'prettier' },
-        graphql = { 'prettier', stop_after_first = true },
-      },
-      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-      formatters = {
-        ['pint'] = {
-          command = 'pint',
-          args = function(_, ctx)
-            return { '--stdin-filename=' .. ctx.filename }
-          end,
-          stdin = true,
-        },
+  },
+  opts = {
+    format_on_save = function()
+      if vim.fn.has 'win32' == 0 then
+        return {
+          timeout_ms = 1000,
+          lsp_format = 'fallback',
+          async = true,
+        }
+      else
+        return {}
+      end
+    end,
+    format_after_save = function()
+      if vim.fn.has 'win32' == 1 then
+        return {}
+      else
+        return {
+          timeout_ms = 1000,
+          lsp_format = 'fallback',
+          async = true,
+        }
+      end
+    end,
+    formatters_by_ft = {
+      rust = { 'rustfmt', lsp_format = 'fallback' },
+      php = { 'pint' },
+      lua = { 'stylua' },
+      javascript = { 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettier', stop_after_first = true },
+      typescript = { 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettier', stop_after_first = true },
+      json = { 'prettier' },
+      graphql = { 'prettier', stop_after_first = true },
+    },
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      ['pint'] = {
+        command = 'pint',
+        args = function(_, ctx)
+          return { '--stdin-filename=' .. ctx.filename }
+        end,
+        stdin = true,
       },
     },
   },
 }
+
+if vim.fn.has 'win32' == 0 then
+  config.opts.format_on_save = {
+    timeout_ms = 1000,
+    lsp_format = 'fallback',
+    async = true,
+  }
+else
+  config.opts.format_after_save = {
+    timeout_ms = 1000,
+    lsp_format = 'fallback',
+    async = true,
+  }
+end
+
+return config
